29fe5ae12f9299a98f5fe2eeab503e58901e1c1b
Dare Hsoe <your-actual-email@example.com>
2025-08-07 15:30:52 +0700

    modifythelandingpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Link from "next/link";
 export default function Home() {
 
   return (
@@ -6,10 +7,19 @@ export default function Home() {
       <main className="flex flex-col  text-center h-dvh justify-center max-w-5xl mx-auto">
         <div className="flex flex-col gap-6 p-12 rounded-xl bg-black/90 sm:max-w-96 mx-auto  text-white sm:text-2xl">
           <h1 className="text-4xl font-bold">KESAN&apos;Employee<br/> Leave Tracker</h1>
-          <address>
-            This is KESAN Employee Leave Tracker System<br/>
-            Only Admin or authenticated staff are allowed to use the system.
-          </address>
+          
+          <div className="flex flex-col gap-4 mt-4">
+            <button className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg transition duration-200">
+              <Link href="/leave_register">
+              Saff Access
+              </Link>
+            </button>
+            <button className="px-6 py-3 bg-green-600 hover:bg-green-700 text-white font-semibold rounded-lg transition duration-200">
+              <Link href="/trackers_submit">
+              Admin Access
+              </Link>            
+            </button>
+          </div>
 
         </div>
       </main>
